8426286e5846e6e8f9390e843e6cde7ce666b588
alienwizard <johanwendin@gmail.com>
2016-08-30 11:53:00 +0200

    init

---
+++ wp-content/themes/design3/searchform.php
@@ -0,0 +1,1 @@
+<form role="search" method="get" id="searchform"    class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">    <div class="searchbar">        <input type="text" value="<?php echo get_search_query(); ?>" placeholder="Sök bland all våra tjänster" name="s" id="s" />                </div></form>

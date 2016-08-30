55d93d965155e505001a6d54c75945509fb2c5fe
alienwizard <johanwendin@gmail.com>
2016-08-30 13:04:22 +0200

    added copy

--- wp-content/themes/design3/footer.php
+++ wp-content/themes/design3/footer.php
@@ -16,7 +16,7 @@
 			<div class="right-field">
 				<h3 class="footer-title">Kontakt</h3>
 				<ul>					<li><span><i></i>Adress:</span><span>Danmarksgatan 32</span></li>					<li><span>Postnummer:</span><span> 753 23</span></li>					<li><span><i></i>Postort</span><span>Uppsala</span></li>					<li><span><i></i>Telefon:</span><span> 018-155605</span></li>					<li><span><i></i>Epost:</span><span><a href="mailto:info@generatorservice.se">info@generatorservice.se</a></span></li>					<li><span>Orgnr<i></i></span><span>5568048119</span></li>					<li><span>Momsnr<i></i></span><span>SE556804811901</span></li>				</ul>				<div class="social-row">				</div>			</div>			<div class="clearfix"></div>		</div>	</div>	<div class="footer-bottom">		<div class="container">			<div class="footer-bottom-cont">
-				<p><i class="fa fa-copyright" aria-hidden="true"></i> Trepe Bilvård AB 2016</p>			</div>		</div>			</div>
+				<p><i class="fa fa-copyright" aria-hidden="true"></i> Generatorservice AB AB 2016</p>			</div>		</div>			</div>
 				<?php wp_footer();?>
 				<script type="text/javascript">
 				</script>
